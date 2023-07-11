580d7acc152815b77c0a2251799d385e4b57ebe5
SurajSinh Rathod <surajsinhrathod75@gmail.com>
2023-07-11 17:47:29 +0530

    Added lazy initialization of retrofit and stackoverflowapi instance

--- app/src/main/java/com/surajrathod/daggerexample/common/composition/AppCompositionRoot.kt
+++ app/src/main/java/com/surajrathod/daggerexample/common/composition/AppCompositionRoot.kt
@@ -1,5 +1,6 @@
 package com.surajrathod.daggerexample.common.composition
 
+import androidx.annotation.UiThread
 import com.surajrathod.daggerexample.Constants
 import com.surajrathod.daggerexample.networking.StackoverflowApi
 import com.surajrathod.daggerexample.questions.FetchQuestionsDetailsUseCase
@@ -7,14 +8,20 @@ import com.surajrathod.daggerexample.questions.FetchQuestionsUseCase
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+@UiThread                   //all the properties in this class will be accessed from the ui thread
 class AppCompositionRoot {
 
-    private val retrofit = Retrofit.Builder()
-        .baseUrl(Constants.BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
 
-    private val stackoverflowApi = retrofit.create(StackoverflowApi::class.java)
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    private val stackoverflowApi by lazy {
+        retrofit.create(StackoverflowApi::class.java)
+    }
     public val fetchQuestionsUseCase get() = FetchQuestionsUseCase(stackoverflowApi)
     // get() means every time this property acceesd it will create new instance of fetchQuestionsUseCase
 
