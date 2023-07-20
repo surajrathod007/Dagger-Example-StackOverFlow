4c9fc0a43ab8df3d44706f4a992a426efc533692
SurajSinh Rathod <surajsinhrathod75@gmail.com>
2023-07-20 11:35:02 +0530

    Used Provider<T> class for getting new instance of imageloader , everytime viewmvc factory is called

--- app/src/main/java/com/surajrathod/daggerexample/screens/common/viewsMvc/ViewMvcFactory.kt
+++ app/src/main/java/com/surajrathod/daggerexample/screens/common/viewsMvc/ViewMvcFactory.kt
@@ -6,15 +6,16 @@ import com.surajrathod.daggerexample.screens.common.imageloader.ImageLoader
 import com.surajrathod.daggerexample.screens.questiondetails.QuestionDetailsViewMvc
 import com.surajrathod.daggerexample.screens.questionslist.QuestionsListMvc
 import javax.inject.Inject
+import javax.inject.Provider
 
-class ViewMvcFactory @Inject constructor(private val layoutInflater: LayoutInflater,private val imageLoader: ImageLoader) {
+class ViewMvcFactory @Inject constructor(private val layoutInflater: LayoutInflater,private val imageLoaderProvider: Provider<ImageLoader>) {
 
     fun newQuestionsListMvc(parent: ViewGroup?): QuestionsListMvc {
         return QuestionsListMvc(layoutInflater, parent)
     }
 
     fun newQuestionDetailsMvc(parent: ViewGroup?) : QuestionDetailsViewMvc{
-        return QuestionDetailsViewMvc(layoutInflater,imageLoader,parent)
+        return QuestionDetailsViewMvc(layoutInflater,imageLoaderProvider.get(),parent)
     }
 
 }
